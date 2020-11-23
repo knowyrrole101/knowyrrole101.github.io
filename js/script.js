8f49c97ef452f88d9cb4730f32ea3a115f76ffa5
Moon Ahmed <moona@indeed.com>
2020-11-22 21:56:18 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var timer;
+var timerStart;
+var timeSpentOnSite = getTimeSpentOnSite();
+
+function getTimeSpentOnSite() {
+    timeSpentOnSite = parseInt(localStorage.getItem('timeSpentOnSite'));
+    timeSpentOnSite = isNaN(timeSpentOnSite) ? 0 : timeSpentOnSite;
+    return timeSpentOnSite;
+}
+
+function startCounting() {
+    document.getElementById('')
+    timerStart = Date.now();
+    timer = setInterval(function() {
+        timeSpentOnSite = getTimeSpentOnSite() + (Date.now() - timerStart);
+        localStorage.setItem('timeSpentOnSite', timeSpentOnSite);
+        timerStart = parseInt(Date.now());
+        // Convert to seconds
+        console.log(parseInt(timeSpentOnSite / 1000));
+    }, 1000);
+}
+startCounting();
